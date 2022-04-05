b9821183f1f8e275099a9b04acc403d2812f47fb
newsbreak-jiaxin <Jiaxin.Cao@gmail.com>
2022-04-05 03:38:39 +0000

    Add classes into init file

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import setup
 
 setup(
     name='tscounters-py',
-    version='0.0.2',
+    version='0.0.4',
     description='Time-series counters',
     author='Jiaxin Cao',
     author_email='Jiaxin.Cao@gmail.com',
     packages=['tscounters'],
     url="https://github.com/jiaxincao/tscounters-py",
     keywords="opentsdb, metrics, counters",
+    install_requires=[
+        "requests",
+    ]
 )
