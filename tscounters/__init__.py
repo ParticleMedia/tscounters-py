a9bcbcac1bda5c34080a23e19701270e7ea47e5a
Jiaxin Cao <Jiaxin.Cao@gmail.com>
2022-05-03 13:07:09 -0700

    Update version

--- tscounters/__init__.py
+++ tscounters/__init__.py
@@ -4,8 +4,5 @@ from .counter_pusher import init_counter_pusher, add_counter_engine
 from .counters import SimpleCounter
 
 
-__version__ = "0.0.2"
-
-
 def init(commit_interval=1):
     init_counter_pusher(commit_interval=commit_interval)
